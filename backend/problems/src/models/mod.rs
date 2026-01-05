3203ee5948fccaf51e2b73b04a81f9c6cd637b5f
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2026-01-05 18:22:26 +0300

    package to handle problems

---
+++ backend/problems/src/models/mod.rs
@@ -0,0 +1,161 @@
+use chrono::NaiveDateTime;
+use diesel::{
+    deserialize::{self, FromSql, FromSqlRow},
+    expression::AsExpression,
+    pg::Pg,
+    prelude::*,
+    serialize::{self, IsNull, Output, ToSql},
+    sql_types::Text,
+};
+use serde::{Deserialize, Serialize};
+use std::io::Write;
+use users::models::User;
+
+#[derive(
+    Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, AsExpression, FromSqlRow, Default,
+)]
+#[diesel(sql_type = Text)]
+pub enum ModeratorFlags {
+    Duplicate,
+    Monitized,
+    Solved,
+    Irrelevant,
+    Inappropriate,
+    #[default]
+    NotReviewed,
+}
+
+impl ToSql<Text, Pg> for ModeratorFlags {
+    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Pg>) -> serialize::Result {
+        let value = match self {
+            ModeratorFlags::Duplicate => "Duplicate",
+            ModeratorFlags::Monitized => "Monitized",
+            ModeratorFlags::Solved => "Solved",
+            ModeratorFlags::Irrelevant => "Irrelevant",
+            ModeratorFlags::Inappropriate => "Inappropriate",
+            ModeratorFlags::NotReviewed => "NotReviewed",
+        };
+        out.write_all(value.as_bytes())?;
+        Ok(IsNull::No)
+    }
+}
+
+impl FromSql<Text, Pg> for ModeratorFlags {
+    fn from_sql(bytes: diesel::pg::PgValue<'_>) -> deserialize::Result<Self> {
+        match bytes.as_bytes() {
+            b"Duplicate" => Ok(ModeratorFlags::Duplicate),
+            b"Monitized" => Ok(ModeratorFlags::Monitized),
+            b"Solved" => Ok(ModeratorFlags::Solved),
+            b"Irrelevant" => Ok(ModeratorFlags::Irrelevant),
+            b"Inappropriate" => Ok(ModeratorFlags::Inappropriate),
+            b"NotReviewed" => Ok(ModeratorFlags::NotReviewed),
+            _ => Err("Unrecognized moderator flags variant".into()),
+        }
+    }
+}
+
+#[derive(
+    Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, AsExpression, FromSqlRow, Default,
+)]
+#[diesel(sql_type = Text)]
+pub enum Category {
+    Technology,
+    Medical,
+    Sport,
+    #[default]
+    NotListed,
+}
+
+impl ToSql<Text, Pg> for Category {
+    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Pg>) -> serialize::Result {
+        let value = match self {
+            Category::Technology => "Technology",
+            Category::Medical => "Medical",
+            Category::Sport => "Sport",
+            Category::NotListed => "NotListed",
+        };
+        out.write_all(value.as_bytes())?;
+        Ok(IsNull::No)
+    }
+}
+
+impl FromSql<Text, Pg> for Category {
+    fn from_sql(bytes: diesel::pg::PgValue<'_>) -> deserialize::Result<Self> {
+        match bytes.as_bytes() {
+            b"Technology" => Ok(Category::Technology),
+            b"Medical" => Ok(Category::Medical),
+            b"Sport" => Ok(Category::Sport),
+            b"NotListed" => Ok(Category::NotListed),
+            _ => Err("Unrecognized category variant".into()),
+        }
+    }
+}
+
+#[derive(
+    Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, AsExpression, FromSqlRow, Default,
+)]
+#[diesel(sql_type = Text)]
+pub enum Status {
+    Trending,
+    HighDemand,
+    #[default]
+    Normal,
+}
+
+impl ToSql<Text, Pg> for Status {
+    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Pg>) -> serialize::Result {
+        let value = match self {
+            Status::Trending => "Trending",
+            Status::HighDemand => "HighDemand",
+            Status::Normal => "Normal",
+        };
+        out.write_all(value.as_bytes())?;
+        Ok(IsNull::No)
+    }
+}
+
+impl FromSql<Text, Pg> for Status {
+    fn from_sql(bytes: diesel::pg::PgValue<'_>) -> deserialize::Result<Self> {
+        match bytes.as_bytes() {
+            b"Trending" => Ok(Status::Trending),
+            b"HighDemand" => Ok(Status::HighDemand),
+            b"Normal" => Ok(Status::Normal),
+            _ => Err("Unrecognized status variant".into()),
+        }
+    }
+}
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = utils::db::schema::featured)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Featured {
+    pub id: i32,
+    pub expired: bool,
+    pub expiring_date: NaiveDateTime,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+}
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = utils::db::schema::problem)]
+#[diesel(belongs_to(User,  foreign_key = user_id))]
+#[diesel(belongs_to(Featured, foreign_key = featured_id))]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Problem {
+    pub id: i32,
+    pub anonymous: bool,
+    pub user_id: i32,
+    pub title: String,
+    pub description: String,
+    pub flag: ModeratorFlags,
+    pub featured_id: Option<i32>,
+    pub category: Category,
+    pub sub_category: Option<String>,
+    pub status: Status,
+    pub public: bool,
+    pub archive: bool,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+}
