1da1411918ca0f4bb172fb0e96e6e66bbbb4414f
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2026-01-05 18:24:11 +0300

    package url scope linkage

--- backend/backend/src/main.rs
+++ backend/backend/src/main.rs
@@ -1,11 +1,6 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, middleware::Logger};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, middleware::Logger, web};
 use env_logger::Env;
-use utils::files::{
-    load_env,
-    types::ENV
-};
-
-
+use utils::config::AppState;
 #[get("/")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
@@ -13,13 +8,20 @@ async fn health_check() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
-    let env: ENV = load_env().unwrap();
-    HttpServer::new(|| {
+    let app_state = web::Data::new(AppState::new());
+    let server_port = app_state.env.server.port;
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(app_state.clone())
+            .service(users::urls::urls())
+            .service(problems::urls::urls())
+            .service(solutions::urls::urls())
+            .service(interactions::urls::urls())
             .service(health_check)
             .wrap(Logger::default())
     })
-    .bind(("0.0.0.0", env.server_port))?
+    .bind(("0.0.0.0", server_port))?
     .run()
     .await
 }
