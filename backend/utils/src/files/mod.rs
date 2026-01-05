0cfb733d13f1761fd8caf6ad1d12a942e874cd98
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2026-01-05 18:25:32 +0300

    database url inclusion

--- backend/utils/src/files/mod.rs
+++ backend/utils/src/files/mod.rs
@@ -1,15 +1,14 @@
 pub mod types;
-use dotenv::dotenv;
-use std::{
-    env,
-    error::Error
-};
-use self::types::ENV;
+use self::types::{ENV, Server};
 use anyhow::Result;
+use dotenv::dotenv;
+use std::env;
 
 pub fn load_env() -> Result<ENV> {
     dotenv().ok();
-    Ok(ENV {
-        server_port: env::var("SERVER_PORT")?.parse()?
-    })
+
+    Ok(ENV::new(
+        env::var("DATABASE_URL")?,
+        Server::new(env::var("SERVER_PORT")?.parse()?),
+    ))
 }
--- backend/utils/src/files/types.rs
+++ backend/utils/src/files/types.rs
@@ -1,3 +1,23 @@
 pub struct ENV {
-    pub server_port: u16
+    pub server: Server,
+    pub database_url: String,
+}
+
+impl ENV {
+    pub fn new(database_url: String, server: Server) -> Self {
+        Self {
+            server,
+            database_url,
+        }
+    }
+}
+
+pub struct Server {
+    pub port: u16,
+}
+
+impl Server {
+    pub fn new(port: u16) -> Self {
+        Self { port }
+    }
 }
