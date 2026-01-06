a341733c8dcd403f9d102fc8a1bb6353bb251ae6
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2026-01-06 11:35:37 +0300

    http to https

--- backend/backend/src/main.rs
+++ backend/backend/src/main.rs
@@ -1,19 +1,33 @@
+pub mod middleware;
+pub mod views;
+pub mod urls;
 use actix_web::{App, HttpResponse, HttpServer, Responder, get, middleware::Logger, web};
 use env_logger::Env;
-use utils::config::AppState;
+use utils::{
+    config::AppState, ssl::ssl_builder_creation
+};
+use self::urls::auth_urls;
+
+
 #[get("/")]
 async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+    HttpResponse::Ok().body("Up")
 }
+
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
     let app_state = web::Data::new(AppState::new());
     let server_port = app_state.env.server.port;
 
+    let builder = ssl_builder_creation().await.expect("Failed to create ssl builder.");
+
     HttpServer::new(move || {
         App::new()
             .app_data(app_state.clone())
+            .service(auth_urls())
             .service(users::urls::urls())
             .service(problems::urls::urls())
             .service(solutions::urls::urls())
@@ -21,7 +35,7 @@ async fn main() -> std::io::Result<()> {
             .service(health_check)
             .wrap(Logger::default())
     })
-    .bind(("0.0.0.0", server_port))?
+    .bind_openssl(format!("0.0.0.0:{:?}", server_port), builder)?
     .run()
     .await
 }
