2f69d157be54f9cc475b15a52cb5ecb594440184
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2026-01-07 17:50:20 +0300

    pagination contruct

--- backend/utils/src/dto/mod.rs
+++ backend/utils/src/dto/mod.rs
@@ -2,6 +2,28 @@ use actix_web::error;
 use derive_more::derive::{Display, Error};
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Pagination {
+    pub page: Option<u32>,
+    pub page_size: Option<u32>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PaginationDb {
+    pub offset: i64,
+    pub limit: i64,
+}
+
+impl Pagination {
+    pub fn limit_and_offset(&self) -> PaginationDb {
+        let offset =
+            (self.page.unwrap_or(1).saturating_sub(1) * self.page_size.unwrap_or(10)) as i64;
+        let limit = self.page_size.unwrap_or(10) as i64;
+
+        PaginationDb { offset, limit }
+    }
+}
+
 #[derive(Debug, Display, Error, Serialize, Deserialize)]
 #[display("Error: {message}")]
 pub struct ErrorResponse {
